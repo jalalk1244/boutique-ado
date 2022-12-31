cfd6b65af28986ccd0014f8a150ee149c4254e03
jalalk1244 <jalaluddin.boss@gmail.com>
2022-12-31 09:35:29 +0000

    Product form

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -7,6 +7,7 @@ from .models import Order, OrderLineItem
 from products.models import Product
 from profiles.models import UserProfile
 
+
 import json
 import time
 
@@ -68,9 +69,9 @@ class StripeWH_Handler:
         profile = None
         username = intent.metadata.username
         if username != 'AnonymousUser':
-            profile.default = UserProfile.object.get(user__username=username)
+            profile = UserProfile.objects.get(user__username=username)
             if save_info:
-                profile.default_Phone_number = shipping_details.phone
+                profile.default_phone_number = shipping_details.phone
                 profile.default_country = shipping_details.address.country
                 profile.default_postcode = shipping_details.address.postal_code
                 profile.default_town_or_city = shipping_details.address.city
@@ -83,11 +84,11 @@ class StripeWH_Handler:
         attempt = 1
         while attempt <= 5:
             try:
-                order = Order.object.get(
+                order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
                     user_profile=profile,
                     email__iexact=billing_details.email,
-                    Phone_number__iexact=shipping_details.phone,
+                    phone_number__iexact=shipping_details.phone,
                     country__iexact=shipping_details.address.country,
                     postcode__iexact=shipping_details.address.postal_code,
                     town_or_city__iexact=shipping_details.address.city,
@@ -100,7 +101,7 @@ class StripeWH_Handler:
                 )
                 order_exists = True
                 break
-            except Order.DoesNotExists:
+            except Order.DoesNotExist:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
@@ -111,20 +112,21 @@ class StripeWH_Handler:
         else:
             order = None
             try:
-                order = Order.objects.create(
-                        full_name=shipping_details.name,
-                        email=billing_details.email,
-                        Phone_number=shipping_details.phone,
-                        country=shipping_details.address.country,
-                        postcode=shipping_details.address.postal_code,
-                        town_or_city=shipping_details.address.city,
-                        street_address1=shipping_details.address.line1,
-                        street_address2=shipping_details.address.line2,
-                        county=shipping_details.address.state,
-                        original_bag=bag,
-                        stripe_pid=pid,
-                    )
-                for item_id, item_data in json.load(bag).items():
+                order = Order.objects.create(	
+                    full_name=shipping_details.name,	
+                    user_profile=profile,	
+                    email=billing_details.email,	
+                    phone_number=shipping_details.phone,	
+                    country=shipping_details.address.country,	
+                    postcode=shipping_details.address.postal_code,	
+                    town_or_city=shipping_details.address.city,	
+                    street_address1=shipping_details.address.line1,	
+                    street_address2=shipping_details.address.line2,	
+                    county=shipping_details.address.state,	
+                    original_bag=bag,	
+                    stripe_pid=pid,	
+                )	
+                for item_id, item_data in json.loads(bag).items():
                     product = Product.objects.get(id=item_id)
                     if isinstance(item_data, int):
                         order_line_item = OrderLineItem(
@@ -144,16 +146,19 @@ class StripeWH_Handler:
                             order_line_item.save()
             except Exception as e:
                 if order:
-                    return HttpResponse(content=f'Webhook received: {event["type"]} | ERROR: {e}')
+                    order.delete()
+                    return HttpResponse(	
+                        content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                        status=500)
         self._send_confirmation_email(order)
-        return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
+        return HttpResponse(	
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',	
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
         '''
         Handle the payment_intent.payment.failed webhook from Stripe
         '''
-        return HttpResponse(
-            content=f'Webhook received: {event["type"]} | SUCCESS: created order in webhook',
+        return HttpResponse(	
+            content=f'Webhook received: {event["type"]}',	
             status=200)
