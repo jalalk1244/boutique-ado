6ba0953bfbfc7c2933ce05f55cefe7274e449302
jalalk1244 <jalaluddin.boss@gmail.com>
2023-01-01 15:05:13 +0000

    Deplyment 2

--- botique_ado/settings.py
+++ botique_ado/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['botique-ado-jalal.herokuapp.com', 'localhost']
-
+ALLOWED_HOSTS = ['boutique-ado-jalal.herokuapp.com', 'localhost']
 
 # Application definition
 
