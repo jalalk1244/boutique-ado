da388e4e46f3293519ab348d623289dd31a9187a
jalalk1244 <jalaluddin.boss@gmail.com>
2023-01-02 17:43:09 +0000

    Change the webhook handler success intent

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -112,20 +112,20 @@ class StripeWH_Handler:
         else:
             order = None
             try:
-                order = Order.objects.create(	
-                    full_name=shipping_details.name,	
-                    user_profile=profile,	
-                    email=billing_details.email,	
-                    phone_number=shipping_details.phone,	
-                    country=shipping_details.address.country,	
-                    postcode=shipping_details.address.postal_code,	
-                    town_or_city=shipping_details.address.city,	
-                    street_address1=shipping_details.address.line1,	
-                    street_address2=shipping_details.address.line2,	
-                    county=shipping_details.address.state,	
-                    original_bag=bag,	
-                    stripe_pid=pid,	
-                )	
+                order = Order.objects.create(
+                    full_name=shipping_details.name,
+                    user_profile=profile,
+                    email=billing_details.email,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.address.country,
+                    postcode=shipping_details.address.postal_code,
+                    town_or_city=shipping_details.address.city,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
+                    county=shipping_details.address.state,
+                    original_bag=bag,
+                    stripe_pid=pid,
+                )
                 for item_id, item_data in json.loads(bag).items():
                     product = Product.objects.get(id=item_id)
                     if isinstance(item_data, int):
